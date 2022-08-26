76d8a99572e6ff4fac5dbf0fbac3a8d373772b1c
Ashwani Ranjan <ranjan062001@gmail.com>
2022-08-26 16:26:35 +0530

    changed student to remove conflict

--- frontend/src/Component/Student.js
+++ frontend/src/Component/Student.js
@@ -1,56 +1,56 @@
-// import React from 'react'
+import React from "react";
 
-// function deleteStudent(id, e){
-//     // axios.delete(`https://localhost:3000/${usn}`)
-//     //   .then(res => {
-//     //     console.log(res);
-//     //     console.log(res.data);
-//     //     const students = this.state.students.filter(item => item.usn !== usn);
-//     //     this.setState({ students });
-//     //   })
+function deleteStudent(id, e) {
+    // axios.delete(`https://localhost:3000/${usn}`)
+    //   .then(res => {
+    //     console.log(res);
+    //     console.log(res.data);
+    //     const students = this.state.students.filter(item => item.usn !== usn);
+    //     this.setState({ students });
+    //   })
+}
 
-// }
+function StudentDetails(props) {
+    return (
+        <div className="post bg-white p-1 my-1">
+            <h4>{propes.name}</h4>
+            <p className="styleDetails">USN = {props.usn}</p>
+            <p className="styleDetails">EMail = {props.email}</p>
+            <p className="styleDetails">Branch = {props.branch}</p>
+            <p className="styleDetails">CGPA = {props.cgpa}</p>
+            <div>
+                <p className="my-1">Delete Student</p>
+                <Fragment>
+                    {!props.auth.loading &&
+                        props.usn === props.auth.student.usn && (
+                            <button
+                                onClick={() => deleteStudent(props.usn)}
+                                type="button"
+                                className="btn btn-danger"
+                            >
+                                <i className="fas fa-times" />
+                            </button>
+                        )}
+                </Fragment>
+            </div>
+        </div>
+    );
+}
 
-// function StudentDetails(props){
-//     return(
-//         <div className="post bg-white p-1 my-1">
-//             <h4>{propes.name}</h4>
-//             <p className='styleDetails'>USN = {props.usn}</p>
-//             <p className='styleDetails'>EMail = {props.email}</p>
-//             <p className='styleDetails'>Branch = {props.branch}</p>
-//             <p className='styleDetails'>CGPA = {props.cgpa}</p>
-//             <div>
-//                 <p className="my-1">Delete Student</p>
-//                 <Fragment>
-//                 {!props.auth.loading && props.usn === props.auth.student.usn && (
-//                     <button
-//                     onClick={() => deleteStudent(props.usn)}
-//                     type="button"
-//                     className="btn btn-danger"
-//                     >
-//                     <i className="fas fa-times" />
-//                     </button>
-//                 )}
-//                 </Fragment>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default class Student extends React.Component {
-//     render(){
-//         return (
-//             <div style={{ width: this.props.width + "px" }}>
-//         <div className="styleStudent">
-//           <StudentDetails
-//             name={this.props.name}
-//             usn = {this.props.usn}
-//             email = {this.props.email}
-//             branch = {this.props.branch}
-//             cgpa = {this.props.cgpa}
-//           />
-//         </div>
-//       </div>
-//         );
-//     }
-// }
+export default class Student extends React.Component {
+    render() {
+        return (
+            <div style={{ width: this.props.width + "px" }}>
+                <div className="styleStudent">
+                    <StudentDetails
+                        name={this.props.name}
+                        usn={this.props.usn}
+                        email={this.props.email}
+                        branch={this.props.branch}
+                        cgpa={this.props.cgpa}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
